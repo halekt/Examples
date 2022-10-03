13d608402aa94a5982fd5738e0fba67ad8a34744
unknown <ivfadeev.ekb@gmail.com>
2022-10-03 20:19:33 +0500

    Генератор псевдослучайных чисел

---
+++ different_005/Program.cs
@@ -0,0 +1,8 @@
+﻿// Генератор псевдослучайных чисел
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
