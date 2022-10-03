62046235751b6d2b2fde6c1e85dd17fbc5ca0494
Илья Фадеев <fadeev_i@ronees.unets.ru>
2022-10-03 10:25:45 +0500

    умножение и деление чисел

---
+++ different_001/Program.cs
@@ -0,0 +1,15 @@
+﻿//
+Console.WriteLine("Введите числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int size = 2; // для a,b
+int sum = 0;
+int avg = 0;
+int index = 0;
+while (index < size)
+{
+    sum = a * b;
+    
+   index++;
+}
+Console.WriteLine($"Среднее арифметическое число {a} умножить на {b} равно {avg=sum/size}");
