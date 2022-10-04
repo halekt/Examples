99621ddc0287eb91f306ca4b381c2c7640516e31
Илья Фадеев <fadeev_i@ronees.unets.ru>
2022-10-04 15:26:51 +0500

    алгоритм массива

---
+++ different_010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Алгоритм масссива 
+
+int[] array = {1, 12, 31, 4, 50, 16, 17, 18};
+
+int n = array.Length;
+int find = 50;
+
+int index = 0;
+
+while( index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
