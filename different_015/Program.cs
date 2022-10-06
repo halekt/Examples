a67784dbfac0f3032a7384b596d1766f44922140
Илья Фадеев <fadeev_i@ronees.unets.ru>
2022-10-06 16:02:02 +0500

    Выводит случайное число из отрезка [10,9999] и показывает наибольшую цифру числа

---
+++ different_015/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выводит случайное число из отрезка [10,9999] и показывает наибольшую цифру числа
+
+int num = new Random ().Next(1, 9999);
+Console.WriteLine(num);
+
+int max = 0;
+int temp =0;
+int temp2=0;
+
+while( num != 0) // != означает не равно
+{
+    temp = num%10; // %10 выводит последнюю цифру числа
+    if(temp > max) max = temp;
+    num = num/10; //
+}
+
+Console.WriteLine(max);
