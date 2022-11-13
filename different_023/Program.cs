7d449263a11cb0a256c91dd296541cd5ccaf0102
halekt <ivfadeev.ekb@gmail.com>
2022-11-13 12:25:59 +0500

    Рекурсия Фибоначи

---
+++ different_023/Program.cs
@@ -0,0 +1,12 @@
+﻿// Рекурсия Фибоначи
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2); // иначе возвращенное число n складываем число  плюс предыдущее число
+}
+
+for(int i =1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}"); 
+}
