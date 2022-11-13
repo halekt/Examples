ba376468dbd48139f9ab67c80ca90d87998572f8
halekt <ivfadeev.ekb@gmail.com>
2022-11-13 16:10:45 +0500

    Задача с буквами и словами

---
+++ different_028/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача с буквами и словами 
+int n = 1;
+void FindWords(string alphabet, char [] word, int lenght =0)
+{
+    if (lenght == word.Length)
+    {
+        System.Console.WriteLine($" {n++} {new String(word)}"); return;
+    }
+
+for (int i = 0; i < alphabet.Length; i++)
+{
+    word[lenght] = alphabet[i];
+    FindWords(alphabet, word, lenght +1);
+}
+}
+FindWords("аисв", new char[2]);
